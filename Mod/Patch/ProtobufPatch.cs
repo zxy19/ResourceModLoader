7827713bcbac6ea0fc819e424cd1c9c009c3f555
xypp <2952795729@qq.com>
2026-03-24 14:59:15 +0800

    临时提交

--- Mod/Patch/ProtobufPatch.cs
+++ Mod/Patch/ProtobufPatch.cs
@@ -38,10 +38,35 @@ namespace ResourceModLoader.Mod.Patch
                 var p1 = p[0].Trim();
                 var target = p[1].Trim();
 
+                bool isAdd = false;
+                int toAddId = -1;
+                int lastPath = -1;
+                if (p1.EndsWith("*"))
+                {
+                    isAdd = true;
+                    p1 = p1.Substring(0,p1.Length - 1);
+                    if (p1.EndsWith("[]"))
+                    {
+                        p1 = p1.Substring(0, p1.Length - 2);
+                        string last= p1.Substring(p1.LastIndexOf(".") + 1);
+                        lastPath= int.Parse(last);
+                    }
+                    else if (p1.EndsWith("]"))
+                    {
+                        Report.Warning(source, $"文本修补 {p1} 的新键语法不被支持");
+                        continue;
+                    }
+                    else
+                    {
+                        string toAdd = p1.Substring(p1.LastIndexOf(".") + 1);
+                        toAddId = int.Parse(toAdd);
+                        p1 = p1.Substring(0, p1.LastIndexOf("."));
+                    }
+                }
                 try
                 {
                     var result = protoObject.Query([p1]);
-                    if (result.Length != 1)
+                    if (result.Length != 1 || (result.Length != 0 && lastPath != -1))
                     {
                         Report.Warning(source, $"文本修补 {p1} 对应 {result.Length} 个候选，已跳过");
                         continue;
@@ -53,7 +78,21 @@ namespace ResourceModLoader.Mod.Patch
                     else
                         bytes = Encoding.UTF8.GetBytes(target);
 
-                    r.Replace(new ReaderMessage(bytes, r.Path, r.Parent));
+                    if(!isAdd)
+                        r.Replace(new ReaderMessage(bytes, r.Path, r.Parent));
+                    else
+                    {
+                        if(lastPath != -1)
+                        {
+                            var sub = ((ReaderMessage)(r.Parent)).Sub[lastPath];
+                            sub.Add(new ReaderMessage(bytes, $"{r.Parent.Path}.{lastPath}.{sub.Count}", r.Parent));
+                            r.Parent.MarkChildReplaced();
+                        }else if(toAddId != -1)
+                        {
+                            r.Sub[toAddId]=new List<ReaderBase> { new ReaderMessage(bytes, $"{r.Parent.Path}.{lastPath}.{0}", r.Parent) };
+                            r.MarkChildReplaced();
+                        }
+                    }
 
                     hasPat = true;
                 }
@@ -64,10 +103,10 @@ namespace ResourceModLoader.Mod.Patch
             }
             return hasPat;
         }
-
         public void Finalize(AssetsManager manager, AssetsFileInstance assets, AssetFileInfo file)
         {
             field["m_Script"].AsByteArray = protoObject.GetBytes() ;
+            File.WriteAllBytes("1.buf", protoObject.GetBytes());
             file.SetNewData(field);
         }
     }
