e6a7e890449898d77151e9cfaace96bb0288119c
xypp <2952795729@qq.com>
2026-09-02 00:22:04 +0800

    提升版本

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace ResourceModLoader
 {
     class Program
     {
-        public static string VERSION = "0.2.4";
+        public static string VERSION = "0.2.5";
         public static GameModder Modder;
         public static bool isDevMode = false;
         static void Main(string[] args)
